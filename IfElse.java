9e1f1aa86b2ec4477a5f5ee1aa2d80c24592b469
Rodrigo Bossini <professorbossini@gmail.com>
2022-03-21 21:39:47 -0300

    IfElse terminado

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota"));
+        if (nota >= 70)
+            JOptionPane.showMessageDialog(null, "Aprovado");
+        else
+            JOptionPane.showMessageDialog(null, "Reprovado");
+    }
+}
